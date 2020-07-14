58d24ffab28041cde984135341287101062fdcb5
carwbrown <carwbrown@google.com>
2020-07-14 15:30:38 -0400

    style cards for rick and morty api

--- src/components/InformationCard/index.jsx
+++ src/components/InformationCard/index.jsx
@@ -1,14 +1,28 @@
-import React from 'react';
-import './Card.css';
+import React from "react";
+import moment from "moment";
+import "./Card.css";
 
-const InformationCard = (props) =>{
-	const {name, id, image} = props;
-	return (
-		<div className="CardWrapper" key={id}>
-			<img className="CardImage" src={image} alt={`${name}`} />
-			<div>{name}</div>
-		</div>
-	)
+const InformationCard = ({
+  name,
+  id,
+  image,
+  origin,
+  status,
+  currentLocation,
+  created
+}) => {
+  return (
+    <div className="CardWrapper" key={id}>
+      <img className="CardImage" src={image} alt={`${name}`} />
+      <div className="CardText">
+        <div className="CardTitle">{name}</div>
+        <div className="CardDescription">
+          {status === "unknown" ? "Unknown" : status} on {currentLocation}.
+          Added {moment(created).format("L")}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default InformationCard;
--- src/containers/Characters/index.jsx
+++ src/containers/Characters/index.jsx
@@ -1,77 +1,99 @@
-import React, { useEffect, useCallback, useState } from 'react';
-import InformationCard from '../../components/InformationCard';
-import usePagination from '../../hooks/usePagination'
+import React, { useEffect, useCallback, useState } from "react";
+import InformationCard from "../../components/InformationCard";
+import usePagination from "../../hooks/usePagination";
 
-const fetchCharacters = async(currentPage) => {
-	const characterDataRes  = await fetch(`https://rickandmortyapi.com/api/character/?page=${currentPage}`);
-	const getCharacterData = await characterDataRes.json();
-	return getCharacterData;
+const fetchCharacters = async currentPage => {
+  const characterDataRes = await fetch(
+    `https://rickandmortyapi.com/api/character/?page=${currentPage}`
+  );
+  const getCharacterData = await characterDataRes.json();
+  console.log("getCharacterData: ", getCharacterData);
+  return getCharacterData;
 };
 
-const Characters = () =>{
-	const [loading, setLoading] = useState(false);
-	const [characters, setCharacters] = useState([]);
-	const [saveTimeout, setSaveTimeout] = useState();
+const Characters = () => {
+  const [loading, setLoading] = useState(false);
+  const [characters, setCharacters] = useState([]);
+  const [saveTimeout, setSaveTimeout] = useState();
 
-	const { currentPage, jumpTo, maxPage, next, prev, setMaxPage } = usePagination();
+  const {
+    currentPage,
+    jumpTo,
+    maxPage,
+    next,
+    prev,
+    setMaxPage
+  } = usePagination();
 
-	const fetchData = useCallback(async () => {
-		setLoading(true);
-		const fetchedCharacters = await fetchCharacters(currentPage);
-		setCharacters(fetchedCharacters.results);
-		setMaxPage(fetchedCharacters.info.pages)
-		if (fetchedCharacters){
-			setLoading(false);
-		}
-		// eslint-disable-next-line
-	}, [])
+  const fetchData = useCallback(async () => {
+    setLoading(true);
+    const fetchedCharacters = await fetchCharacters(currentPage);
+    setCharacters(fetchedCharacters.results);
+    setMaxPage(fetchedCharacters.info.pages);
+    if (fetchedCharacters) {
+      setLoading(false);
+    }
+    // eslint-disable-next-line
+  }, []);
 
-	const fetchDataPagination = useCallback(async () => {
-		const fetchedCharacters = await fetchCharacters(currentPage);
-		setCharacters(fetchedCharacters.results);
-	}, [currentPage])
+  const fetchDataPagination = useCallback(async () => {
+    const fetchedCharacters = await fetchCharacters(currentPage);
+    setCharacters(fetchedCharacters.results);
+  }, [currentPage]);
 
-	useEffect(() => {
-		fetchData();
-	}, [fetchData])
+  useEffect(() => {
+    fetchData();
+  }, [fetchData]);
 
-	useEffect(() => {
-		fetchDataPagination();
-	}, [fetchDataPagination])
+  useEffect(() => {
+    fetchDataPagination();
+  }, [fetchDataPagination]);
 
-	if (loading){
-		return <div>Loading Characters...</div>
-	}
+  if (loading) {
+    return <div>Loading Characters...</div>;
+  }
 
-	const handleOnChange = (event) => {
-		clearTimeout(saveTimeout);
-		const newPage = event.target.value;
+  const handleOnChange = event => {
+    clearTimeout(saveTimeout);
+    const newPage = event.target.value;
 
-		setSaveTimeout(setTimeout(() => {
-			jumpTo(newPage);
-		}, 1000))
-	}
+    setSaveTimeout(
+      setTimeout(() => {
+        jumpTo(newPage);
+      }, 1000)
+    );
+  };
   return (
-		<div>
-			<h2>Characters</h2>
-			<button onClick={prev} disabled={currentPage === 1}>Prev</button>
-			<button onClick={next} disabled={currentPage === maxPage}>Next</button>
-			<div>on page: {currentPage}</div>
-			<div>
-				Jump to Page: <input name="pageNumber" onChange={handleOnChange}/>
-			</div>
-			<div className="InformationSection">
-				{characters.map(character => (
-					<InformationCard
-						key={character.id}
-						id={character.id}
-						name={character.name}
-						image={character.image}	
-					/>
-				))}
-			</div>
-		</div>
+    <div>
+      <h2>Characters</h2>
+      <button onClick={prev} disabled={currentPage === 1}>
+        Prev
+      </button>
+      <button onClick={next} disabled={currentPage === maxPage}>
+        Next
+      </button>
+      <div>
+        on page: <strong>{currentPage}</strong> of <strong>{maxPage}</strong>
+      </div>
+      <div>
+        Jump to Page: <input name="pageNumber" onChange={handleOnChange} />
+      </div>
+      <div className="InformationSection">
+        {characters.map(character => (
+          <InformationCard
+            key={character.id}
+            id={character.id}
+            name={character.name}
+            image={character.image}
+            status={character.status}
+            origin={character.origin}
+            currentLocation={character.location.name}
+            created={character.created}
+          />
+        ))}
+      </div>
+    </div>
   );
-}
+};
 
 export default Characters;
